19c73b0904f32cf2b2d516da2e7871c86b1fc5bd
LIU JIAN <onanying@126.com>
2020-08-26 21:18:06 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,17 @@
+## Mix API Skeleton
+
+框架 API 骨架代码，包含很多代码范例，开箱即用
+
+- 安装开发工具
+
+~~~
+git clone https://github.com/mix-go/mix
+cd mix
+go install
+~~~
+
+- 创建应用骨架
+
+~~~
+mix api --name=hello
+~~~
---
+++ manifest/manifest.go
@@ -0,0 +1,24 @@
+package manifest
+
+import (
+    "github.com/mix-go/mix-api-skeleton/manifest/beans"
+    "github.com/mix-go/mix-api-skeleton/manifest/commands"
+    "github.com/mix-go/console"
+    "github.com/mix-go/dotenv"
+)
+
+var (
+    ApplicationDefinition console.ApplicationDefinition
+)
+
+func Init() {
+    beans.Init()
+
+    ApplicationDefinition = console.ApplicationDefinition{
+        AppName:    "app",
+        AppVersion: "1.0.0-alpha",
+        AppDebug:   dotenv.Getenv("APP_DEBUG").Bool(),
+        Beans:      beans.Beans,
+        Commands:   commands.Commands,
+    }
+}
