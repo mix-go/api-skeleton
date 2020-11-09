cf280f673383b3c774a079ed9f0c07b1e09fb765
LIU JIAN <onanying@126.com>
2020-11-09 15:31:03 +0800

    feat: update

--- manifest/commands/all.go
+++ manifest/commands/all.go
@@ -1,30 +1,9 @@
 package commands
 
 import (
-    "github.com/mix-go/api-skeleton/commands"
     "github.com/mix-go/console"
 )
 
 var (
     Commands []console.CommandDefinition
 )
-
-func init() {
-    Commands = append(Commands,
-        console.CommandDefinition{
-            Name:  "api",
-            Usage: "\tStart the api server",
-            Options: []console.OptionDefinition{
-                {
-                    Names: []string{"a", "addr"},
-                    Usage: "\tListen to the specified address",
-                },
-                {
-                    Names: []string{"d", "daemon"},
-                    Usage: "\tRun in the background",
-                },
-            },
-            Command: &commands.APICommand{},
-        },
-    )
-}
